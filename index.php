d9e2fc859945800d3294af5a6176ade08faa71e1
Christian Cutter <christianbcutter@yahoo.com>
2025-05-29 19:51:19 -0700

    Add initial theme files for Greg Stuart Urban Design

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) :
+                the_post();
+                the_title('<h1>', '</h1>');
+                the_content();
+            endwhile;
+        else :
+            echo '<p>No content found.</p>';
+        endif;
+        ?>
+    </main>
+</div>
+
+<?php get_footer(); ?>
